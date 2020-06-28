34941cc6051408ccb21dd348efaa474ed6c4f90b
Makito <sumimakito@hotmail.com>
2020-06-28 21:36:08 +0800

    Updated the dev server

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import Koa = require("koa");
 import Router = require("koa-router");
 
+import koaBodyParser = require("koa-bodyparser");
 import koaJson = require("koa-json");
 import koaServe = require("koa-static");
 import koaSend = require("koa-send");
@@ -24,12 +25,16 @@ react.use(reactRouter.routes());
 react.use(reactRouter.allowedMethods());
 
 const app = new Koa();
+app.use(koaBodyParser());
 app.use(koaJson({ pretty: false }));
 app.use(koaMount("/", react));
 
 const router = new Router();
 router.get("/tickets", async (ctx) => {
   ctx.body = getTickets();
+});
+router.post("/order", async (ctx) => {
+  ctx.body = { result: "ok", ...ctx.request.body };
 });
 app.use(router.routes());
 app.use(router.allowedMethods());
